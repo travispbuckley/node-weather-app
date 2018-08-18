e4f4cfb6357ea729684edfc0404e34f94835ee3c
travispbuckley <travispbuckley@lewisu.edu>
2018-08-18 07:57:26 -0500

    use results from google api to display address

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const request = require('request');
 
+// 4711 Yender Avenue
+
 request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=4711%20Yender%20Avenue%20Lisle',
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    // console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Lat: ${body.results[0].geometry.location.lat}`);
+    console.log(`Long: ${body.results[0].geometry.location.lng}`);
 });
